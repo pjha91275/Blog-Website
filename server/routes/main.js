e7a87a5c6da6d34b7be8f914d7a6a76d8cce0d95
pjha91275 <pjha91275@gmail.com>
2025-08-03 01:24:09 +0530

    Completed Blog Website 60%

--- server/routes/main.js
+++ server/routes/main.js
@@ -81,6 +81,35 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+/**
+ * POST /
+ * Post - searchTerm
+ */
+
+router.post('/search', async (req, res) => {
+  try{
+    const locals = {
+    title: "Search",
+    description: "A simple blog built with NodeJS,Express and MongoDB"
+  };
+
+  let searchTerm = req.body.searchTerm;
+  const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, '');
+
+  const data = await Post.find({
+    $or: [
+      { title: { $regex: new RegExp(searchNoSpecialChar, 'i' ) } },
+      { body: { $regex: new RegExp(searchNoSpecialChar, 'i' ) } }
+    ]
+  })
+
+    res.render("search", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 
 router.get('/about', (req, res) => {
   res.render('about');
